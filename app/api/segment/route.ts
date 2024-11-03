4c3541d632b574336c0f3dfc0ad57e2eb465b439
TragZa <46190954+TragZa@users.noreply.github.com>
2024-11-03 16:26:55 +0530

    .

--- app/api/segment/route.ts
+++ app/api/segment/route.ts
@@ -31,7 +31,7 @@ export async function POST(request: Request) {
       await new Promise((resolve) => setTimeout(resolve, 10000));
     }
 
-    const response = await fetch("https://0015-2405-201-34-b121-9c0f-5b10-a71d-b87e.ngrok-free.app/api/segment", {
+    const response = await fetch("https://c263-2405-201-34-b121-6c23-82ad-245f-9a44.ngrok-free.app/api/segment", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
